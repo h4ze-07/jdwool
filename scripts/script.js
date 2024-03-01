bd201c33a7b4b9b904138b1ad002b82c3766c6d2
Vlad1sl4v <122152653+h4ze-07@users.noreply.github.com>
2024-03-01 15:15:57 +0200

    finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 const burger = document.getElementById('burger');
 const mobileMenu = document.getElementById('mobile-menu');
+const mainBtn = document.querySelector('.main__btn');
 
 const showMobileMenu = () => {
     mobileMenu.classList.toggle('hidden')
     mobileMenu.classList.toggle('show')
 }
 
-
+mainBtn.addEventListener('click', () => {
+    window.location.href = '/pages/catalog.html';
+})
 
