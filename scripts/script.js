0ffd4febdd1c1a5b502a45c731c63b53d553186f
Vlad1sl4v <122152653+h4ze-07@users.noreply.github.com>
2024-03-01 10:16:45 +0200

    feat: main page

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+const showMobileMenu = () => {
+    mobileMenu.classList.toggle('hidden')
+    mobileMenu.classList.toggle('show')
+}
+
+
+
